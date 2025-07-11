b71539e16b6cd0263f41cecad0ba2bdc5007875f
Melchor Valencia <valencia.mel.06@gmail.com>
2025-07-11 23:41:43 +0800

    fix: if paid, make the color status green

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -27,6 +27,7 @@ export const getStatusColor = (status: Status | string | null): string => {
       return "bg-blue-100 text-blue-800";
     case "completed":
     case "delivered":
+    case "paid":
       return "bg-green-100 text-green-800";
     case "cancelled":
     case "rejected":
